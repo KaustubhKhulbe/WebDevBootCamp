adaa6a69516d9c45a44eb8c669d25c5de395dd5d
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-05 09:19:08 -0700

    dice game finished

--- Dice/index.js
+++ Dice/index.js
@@ -1,2 +1,11 @@
-document.querySelector(".img1").src = "images/dice" + Math.floor(Math.random() * 6 + 1) + ".png";
-document.querySelector(".img2").src = "images/dice" + Math.floor(Math.random() * 6 + 1) + ".png";
+var roll1 =  Math.floor(Math.random() * 6 + 1);
+var roll2 =  Math.floor(Math.random() * 6 + 1);
+
+document.querySelector(".img1").src = "images/dice" + roll1 + ".png";
+document.querySelector(".img2").src = "images/dice" + roll2 + ".png";
+
+var h1 = document.querySelector("h1")
+
+if(roll1 > roll2) h1.textContent = "🚩 Player 1 Wins!";
+else if(roll2 > roll1) h1.textContent = "Player 2 Wins! 🚩";
+else h1.textContent = "Draw"
