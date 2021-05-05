4684c3b84a74384058acb9dbdcf817eee88919fc
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-05 01:02:17 -0700

    dom

---
+++ Dice/index.js
@@ -0,0 +1,2 @@
+document.querySelector(".img1").src = "images/dice" + Math.floor(Math.random() * 6 + 1) + ".png";
+document.querySelector(".img2").src = "images/dice" + Math.floor(Math.random() * 6 + 1) + ".png";
