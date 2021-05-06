2e14da75bac430f40a78174ce365ec27b0328b02
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-05 18:59:33 -0700

    drum kit site progress

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,4 +1,32 @@
 document.querySelectorAll(".drum").forEach(function(btn){
-     btn.addEventListener("click", function(){alert("I got clicked");})
+     btn.addEventListener("click", function(){
+               switch(btn.innerHTML){
+                    case "w":
+                         new Audio('sounds/tom-1.mp3').play();
+                         break;
+                    case "a":
+                         new Audio('sounds/tom-2.mp3').play();
+                         break;
+                    case "s":
+                         new Audio('sounds/tom-3.mp3').play();
+                         break;
+                    case "d":
+                         new Audio('sounds/tom-4.mp3').play();
+                         break;
+                    case "j":
+                         new Audio('sounds/crash.mp3').play();
+                         break;
+                    case "k":
+                         new Audio('sounds/kick-bass.mp3').play();
+                         break;
+                    case "l":
+                         new Audio('sounds/snare.mp3').play();
+                         break;
+                    default:
+                         
+               }
+          })
      }
 )
+
+// new Audio('sounds/tom-1.mp3').play();
