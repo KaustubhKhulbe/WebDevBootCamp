1cf7419353011350eac0801d82e0ec5c38b0fd14
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-06 00:46:32 -0700

    added keyboard sound listener

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,32 +1,37 @@
 document.querySelectorAll(".drum").forEach(function(btn){
      btn.addEventListener("click", function(){
-               switch(btn.innerHTML){
-                    case "w":
-                         new Audio('sounds/tom-1.mp3').play();
-                         break;
-                    case "a":
-                         new Audio('sounds/tom-2.mp3').play();
-                         break;
-                    case "s":
-                         new Audio('sounds/tom-3.mp3').play();
-                         break;
-                    case "d":
-                         new Audio('sounds/tom-4.mp3').play();
-                         break;
-                    case "j":
-                         new Audio('sounds/crash.mp3').play();
-                         break;
-                    case "k":
-                         new Audio('sounds/kick-bass.mp3').play();
-                         break;
-                    case "l":
-                         new Audio('sounds/snare.mp3').play();
-                         break;
-                    default:
-                         
-               }
+               playChar(btn.innerHTML);
           })
      }
 )
 
-// new Audio('sounds/tom-1.mp3').play();
+document.addEventListener("keydown", function(e){
+     playChar(e.key);
+})
+
+function playChar(char){
+     switch(char){
+          case "w":
+               new Audio('sounds/tom-1.mp3').play();
+               break;
+          case "a":
+               new Audio('sounds/tom-2.mp3').play();
+               break;
+          case "s":
+               new Audio('sounds/tom-3.mp3').play();
+               break;
+          case "d":
+               new Audio('sounds/tom-4.mp3').play();
+               break;
+          case "j":
+               new Audio('sounds/crash.mp3').play();
+               break;
+          case "k":
+               new Audio('sounds/kick-bass.mp3').play();
+               break;
+          case "l":
+               new Audio('sounds/snare.mp3').play();
+               break;
+          default:
+     }
+}
