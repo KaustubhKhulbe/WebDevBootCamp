d0e70675202a46218af8a7e3fbeded6d1e3fc50c
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-08 11:02:26 -0700

    finished express, began api

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", function(req, res){
-     res.send("Hello World");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/bmicalculator", function(req, res){
+     res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res){
+     var weight = Number(req.body.weight);
+     var height = Number(req.body.height);
+     const result = weight / Math.pow(height, 2);
+     res.send("Result is: " + result);
 })
 
 app.listen(3000, function(){
