6784ee8ea04633bf779704e6fd8643ed45a75259
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-09 12:57:44 -0700

    hooked up with mailchimp

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const https = require("https");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
@@ -10,9 +11,42 @@ app.listen(3000, function(){
 })
 
 app.post("/", function(req, res){
-     var firstName = req.body.fName;
-     var lastName = req.body.lName;
-     var email = req.body.email;
+     const firstName = req.body.fName;
+     const lastName = req.body.lName;
+     const email = req.body.email;
+
+     const data = {
+          members: [
+               {
+                    email_address: email,
+                    status: "subscribed",
+                    merge_fields: {
+                         FNAME: firstName,
+                         LNAME: lastName
+                    }
+               }
+          ]
+     }
+
+     const jsonData = JSON.stringify(data);
+
+     const url = "https://us1.api.mailchimp.com/3.0/lists/49730c5d9f"
+
+     const options = {
+          method: "POST",
+          auth: "kaustubh1:1f973c184463e470e278446647b58b96-us1"
+     }
+
+     const request = https.request(url, options, function(response){
+          if(response.statusCode === 200) res.send("Successs");
+          else res.send("failure");
+          response.on("data", function(data){
+               console.log(JSON.parse(data));
+          })
+     })
+
+     request.write(jsonData);
+     request.end();
 
      console.log(firstName + " " + lastName + " " + email);
 })
@@ -20,3 +54,6 @@ app.post("/", function(req, res){
 app.get("/", function(req, res){
      res.sendFile(__dirname + "/signup.html");
 })
+
+// 1f973c184463e470e278446647b58b96-us1
+// 49730c5d9f
